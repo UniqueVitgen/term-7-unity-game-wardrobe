0d08822827cd30275031929325e8fce0a275bf99
vitali <vitgen1997@mail.ru>
2018-10-24 11:16:18 +0300

    first commit

---
+++ Assets/scripts/RigidBodyController.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RigidBodyController : MonoBehaviour {
+	Rigidbody rb;
+	public float speed;
+
+	void Start () {
+		rb = GetComponent<Rigidbody>();
+	}
+	void FixedUpdate () {
+		float mH = Input.GetAxis ("Horizontal");
+		float mV = Input.GetAxis ("Vertical");
+		rb.velocity = new Vector3 (mH * speed, rb.velocity.y, mV * speed);
+	}
+}
