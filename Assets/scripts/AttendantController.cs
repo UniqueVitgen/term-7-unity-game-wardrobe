020d51580bda60ca0316c8533cca9865f0b44ac0
vitali <vitgen1997@mail.ru>
2018-10-26 12:55:48 +0300

    nikita please relaxe

---
+++ Assets/scripts/AttendantController.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class AttendantController : MonoBehaviour {
+
+	NavMeshAgent nNavMeshAgent;
+	public Camera camera;
+
+	// Use this for initialization
+	void Start () {
+		nNavMeshAgent = GetComponent<NavMeshAgent> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (camera != null) {
+			Ray ray = camera.ScreenPointToRay (Input.mousePosition);
+
+			RaycastHit hit;
+
+			if (Input.GetMouseButtonDown (0)) {
+				if (Physics.Raycast (ray, out hit, 100)) {
+					nNavMeshAgent.destination = hit.point;
+				}
+			}
+
+			if (nNavMeshAgent.remainingDistance < nNavMeshAgent.stoppingDistance) {
+			} else {
+			}
+		}
+	}
+}
